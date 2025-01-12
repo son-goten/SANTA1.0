14b35ec3a49fcbc73339b4003a979a2ffe279a93
Hyun7en <rhqudgus99@gmail.com>
2025-01-12 14:12:20 +0900

    [Feat] 주문 관리 기능 구현 v2.0

--- src/main/java/com/example/santa/order/vo/OrderVO.java
+++ src/main/java/com/example/santa/order/vo/OrderVO.java
@@ -1,20 +1,22 @@
 package com.example.santa.order.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderVO {
-
     private int orderId;
     private int warehouseId;
     private int branchId;
-    private Date orderDate;
+    private int productId;
+    private int quantity;
     private String orderStatus;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime orderDate;
 
 }
