650fa76cd9b6c46b288dd2b14ce750a1994d19b2
Geunyeong Kim <98966788+Dubutoto@users.noreply.github.com>
2025-01-13 17:52:05 +0900

    [Feat] 페이지 접근권한 구현 완료

--- src/main/java/com/example/santa/home/controller/HomeController.java
+++ src/main/java/com/example/santa/home/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.santa;
+package com.example.santa.home.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index"; // templates/index.html 파일을 렌더링
+    @GetMapping("/home")
+    public String homePage() {
+        return "home/home"; // home.html 파일을 렌더링
     }
 }
